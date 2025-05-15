caf6c28e4a804f437ead2d18fcd74c285160e04a
Jetross <jetrossneri07@gmail.com>
2025-05-15 08:31:36 +0800

    added home page in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '@/views/Forms/LoginForm.vue'
 import RegisterForm from '@/views/Forms/RegisterForm.vue'
+import HomePage from '@/views/Pages/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterForm,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomePage,
     },
   ],
 })
