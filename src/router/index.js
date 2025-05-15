8830869e3fca6f1109b1b7d98bee094dcca5fc73
Jetross <jetrossneri07@gmail.com>
2025-05-15 08:27:30 +0800

    added register form in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '@/views/Forms/LoginForm.vue'
+import RegisterForm from '@/views/Forms/RegisterForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginForm,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterForm,
     },
   ],
 })
