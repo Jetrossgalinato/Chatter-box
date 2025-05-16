58a5c061385724b21ddd5f150148c4db34675cd4
Jetross <jetrossneri07@gmail.com>
2025-05-16 14:59:17 +0800

    added style in card

--- src/views/Forms/LoginForm.vue
+++ src/views/Forms/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     href="#"
-    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    class="card block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
   >
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       Noteworthy technology acquisitions 2021
@@ -13,4 +13,10 @@
   </a>
 </template>
 <script setup></script>
-<style scoped></style>
+<style scoped>
+.card {
+  margin-top: 15%;
+  margin-left: 35%;
+  padding: 3%;
+}
+</style>
