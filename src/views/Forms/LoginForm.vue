b05c1c3ed55067e10249e5735cb9cde336a8d2fd
Jetross <jetrossneri07@gmail.com>
2025-05-17 10:21:57 +0800

    centered the card

--- src/views/Forms/LoginForm.vue
+++ src/views/Forms/LoginForm.vue
@@ -1,22 +1,19 @@
 <template>
-  <a
-    href="#"
-    class="card block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-  >
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      Chatter-Box
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-      chronological order.
-    </p>
-  </a>
+  <div class="flex items-center justify-center min-h-screen">
+    <a
+      href="#"
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Chatter-Box
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+        chronological order.
+      </p>
+    </a>
+  </div>
 </template>
+
 <script setup></script>
-<style scoped>
-.card {
-  margin-top: 15%;
-  margin-left: 35%;
-  padding: 3%;
-}
-</style>
+<style scoped></style>
